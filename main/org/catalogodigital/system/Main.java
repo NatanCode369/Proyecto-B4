fd9e286608a9ae1a90bd391821130ca3da27431b
bvasquez-byte <bvasquezquel@gmail.com>
2026-09-02 10:50:03 -0600

    creación de las clases que ejecutarán el programa.

---
+++ main/org/catalogodigital/system/Main.java
@@ -0,0 +1,15 @@
+package org.catalogodigital.system;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(Application.class, args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+
+    }
+}
