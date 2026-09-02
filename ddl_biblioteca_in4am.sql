408d2392d058897667e9a71a94eb01dc8515f061
edubon-2023037 <edubon-2023037@kinal.edu.gt>
2026-09-02 11:57:57 -0600

    fix: Arreglados los tipos de datos en la database
    - se cambiaron los varchar por int en los id
    - los id ahora usan auto_increment
    - se agrego los procedimientos almacenados para logins
    - agregadas contraseñas para todos los usuarios

--- ddl_biblioteca_in4am.sql
+++ ddl_biblioteca_in4am.sql
@@ -3,27 +3,29 @@ create database catalogo_digital_in4am;
 use catalogo_digital_in4am;
 
 create table Estudiante(
-    ID_estudiante varchar(36) not null,
+    ID_estudiante int not null auto_increment,
     nombre_estudiante varchar(50) not null,
     apellido_estudiante varchar(50) not null,
+    correo_estudiante varchar(50) not null,
+    contrasena_estudiante varchar(50) not null,
     constraint pk_estudiante primary key (ID_estudiante)
 );
 
 create table Jefe(
-    ID_jefe varchar(36) not null,
+    ID_jefe int not null auto_increment,
     nombre_jefe varchar(50) not null,
     apellido_jefe varchar(50) not null,
-    correo varchar(50) not null,
-    contrasena varchar(30) not null,
+    correo_jefe varchar(50) not null,
+    contrasena_jefe varchar(30) not null,
     constraint pk_jefe primary key (ID_jefe)
 );
 
 create table Bibliotecario(
-    ID_bibliotecario varchar(36) not null,
+    ID_bibliotecario int not null auto_increment,
     nombre_bibliotecario varchar(50) not null,
     apellido_bibliotecario varchar(50) not null,
-    correo varchar(50) not null,
-    contrasena varchar(30) not null,
+    correo_bibliotecario varchar(50) not null,
+    contrasena_bibliotecario varchar(30) not null,
     constraint pk_bibliotecario primary key (ID_bibliotecario)
 );
 
@@ -38,8 +40,8 @@ create table Libro(
 );
 
 create table Prestamo(
-    ID_prestamo varchar(36) not null,
-    ID_estudiante varchar(36) not null,
+    ID_prestamo int not null auto_increment,
+    ID_estudiante int not null,
     ISBN varchar(13) not null,
     fecha_adquisicion date not null,
     fecha_vencimiento date not null,
@@ -48,6 +50,47 @@ create table Prestamo(
     constraint fk_prestamo_estudiante foreign key (ID_estudiante) references Estudiante(ID_estudiante),
     constraint fk_prestamo_libro foreign key (ISBN) references Libro(ISBN)
 );
+
+-- ----------------------------------LOGIN--------------------------------------------
+
+-- LOGIN JEFE
+Delimiter $$
+create procedure sp_login_jefe(
+    in correo_jefe_p varchar(50),
+    in contrasena_jefe_p varchar(30)
+)
+begin
+    select * from Jefe
+    where correo_jefe = correo_jefe_p
+      and contrasena_jefe = contrasena_jefe_p;
+end$$
+Delimiter ;
+
+-- LOGIN BIBLIOTECARIO
+Delimiter $$
+create procedure sp_login_bibliotecario(
+    in correo_bibliotecario_p varchar(50),
+    in contrasena_bibliotecario_p varchar(30)
+)
+begin
+    select * from Bibliotecario
+    where correo_bibliotecario = correo_bibliotecario_p
+      and contrasena_bibliotecario = contrasena_bibliotecario_p;
+end$$
+Delimiter ;
+
+-- LOGIN ESTUDIANTE
+Delimiter $$
+create procedure sp_login_estudiante(
+    in correo_estudiante_p varchar(50),
+    in contrasena_estudiante_p varchar(50)
+)
+begin
+    select * from Estudiante
+    where correo_estudiante = correo_estudiante_p
+      and contrasena_estudiante = contrasena_estudiante_p;
+end$$
+Delimiter ;
 
 -- -----------------------------------CRUDS Libro-------------------------------------------
  
@@ -118,16 +161,18 @@ Delimiter ;
 -- CREATE
 Delimiter $$
 create procedure sp_create_estudiante(
-    in ID_estudiante_p varchar(36),
     in nombre_estudiante_p varchar(50),
-    in apellido_estudiante_p varchar(50)
+    in apellido_estudiante_p varchar(50),
+    in correo_estudiante_p varchar(50),
+    in contrasena_estudiante_p varchar(50)
 )
 begin
-    insert into Estudiante(ID_estudiante, nombre_estudiante, apellido_estudiante)
-    values(ID_estudiante_p, nombre_estudiante_p, apellido_estudiante_p);
+    insert into Estudiante(nombre_estudiante, apellido_estudiante, correo_estudiante, contrasena_estudiante)
+    values(nombre_estudiante_p, apellido_estudiante_p, correo_estudiante_p, contrasena_estudiante_p);
 end$$
 Delimiter ;
 
+/*
 -- READ
 Delimiter $$
 create procedure sp_read_estudiante()
@@ -135,56 +180,60 @@ begin
     select * from Estudiante;
 end$$
 Delimiter ;
+*/
 
 -- UPDATE
 Delimiter $$
 create procedure sp_update_estudiante(
-    in ID_estudiante_p varchar(36),
+    in ID_estudiante_p int,
     in nombre_estudiante_p varchar(50),
-    in apellido_estudiante_p varchar(50)
+    in apellido_estudiante_p varchar(50),
+    in correo_estudiante_p varchar(50),
+    in contrasena_estudiante_p varchar(50)
 )
 begin
     update Estudiante
     set
         nombre_estudiante = nombre_estudiante_p,
-        apellido_estudiante = apellido_estudiante_p
+        apellido_estudiante = apellido_estudiante_p,
+        correo_estudiante = correo_estudiante_p,
+        contrasena_estudiante = contrasena_estudiante_p
     where ID_estudiante = ID_estudiante_p;
 end$$
 Delimiter ;
 
 -- DELETE
 Delimiter $$
-create procedure sp_delete_estudiante(in ID_estudiante_p varchar(36))
+create procedure sp_delete_estudiante(in ID_estudiante_p int)
 begin
     delete from Estudiante where ID_estudiante = ID_estudiante_p;
 end$$
 Delimiter ;
-
+/*
 -- SEARCH
 Delimiter $$
-create procedure sp_search_estudiante(in ID_estudiante_p varchar(36))
+create procedure sp_search_estudiante(in ID_estudiante_p int)
 begin
     select * from Estudiante where ID_estudiante = ID_estudiante_p;
 end$$
 Delimiter ;
-
+*/
 -- -------------------------------------CRUDS Jefe-------------------------------------------
 
 -- CREATE
 Delimiter $$
 create procedure sp_create_jefe(
-    in ID_jefe_p varchar(36),
     in nombre_jefe_p varchar(50),
     in apellido_jefe_p varchar(50),
-    in correo_p varchar(50),
-    in contrasena_p varchar(30)
+    in correo_jefe_p varchar(50),
+    in contrasena_jefe_p varchar(30)
 )
 begin
-    insert into Jefe(ID_jefe, nombre_jefe, apellido_jefe, correo, contrasena)
-    values(ID_jefe_p, nombre_jefe_p, apellido_jefe_p, correo_p, contrasena_p);
+    insert into Jefe(nombre_jefe, apellido_jefe, correo_jefe, contrasena_jefe)
+    values(nombre_jefe_p, apellido_jefe_p, correo_jefe_p, contrasena_jefe_p);
 end$$
 Delimiter ;
-
+/*
 -- READ
 Delimiter $$
 create procedure sp_read_jefe()
@@ -192,60 +241,59 @@ begin
     select * from Jefe;
 end$$
 Delimiter ;
-
+*/
 -- UPDATE
 Delimiter $$
 create procedure sp_update_jefe(
-    in ID_jefe_p varchar(36),
+    in ID_jefe_p int,
     in nombre_jefe_p varchar(50),
     in apellido_jefe_p varchar(50),
-    in correo_p varchar(50),
-    in contrasena_p varchar(30)
+    in correo_jefe_p varchar(50),
+    in contrasena_jefe_p varchar(30)
 )
 begin
     update Jefe
     set
         nombre_jefe = nombre_jefe_p,
         apellido_jefe = apellido_jefe_p,
-        correo = correo_p,
-        contrasena = contrasena_p
+        correo_jefe = correo_jefe_p,
+        contrasena_jefe = contrasena_jefe_p
     where ID_jefe = ID_jefe_p;
 end$$
 Delimiter ;
 
 -- DELETE
 Delimiter $$
-create procedure sp_delete_jefe(in ID_jefe_p varchar(36))
+create procedure sp_delete_jefe(in ID_jefe_p int)
 begin
     delete from Jefe where ID_jefe = ID_jefe_p;
 end$$
 Delimiter ;
-
+/*
 -- SEARCH
 Delimiter $$
-create procedure sp_search_jefe(in ID_jefe_p varchar(36))
+create procedure sp_search_jefe(in ID_jefe_p int)
 begin
     select * from Jefe where ID_jefe = ID_jefe_p;
 end$$
 Delimiter ;
-
+*/
 -- --------------------------------------CRUDS Bibliotecario----------------------------------
 
 -- CREATE
 Delimiter $$
 create procedure sp_create_bibliotecario(
-    in ID_bibliotecario_p varchar(36),
     in nombre_bibliotecario_p varchar(50),
     in apellido_bibliotecario_p varchar(50),
-    in correo_p varchar(50),
-    in contrasena_p varchar(30)
+    in correo_bibliotecario_p varchar(50),
+    in contrasena_bibliotecario_p varchar(30)
 )
 begin
-    insert into Bibliotecario(ID_bibliotecario, nombre_bibliotecario, apellido_bibliotecario, correo, contrasena)
-    values(ID_bibliotecario_p, nombre_bibliotecario_p, apellido_bibliotecario_p, correo_p, contrasena_p);
+    insert into Bibliotecario(nombre_bibliotecario, apellido_bibliotecario, correo_bibliotecario, contrasena_bibliotecario)
+    values(nombre_bibliotecario_p, apellido_bibliotecario_p, correo_bibliotecario_p, contrasena_bibliotecario_p);
 end$$
 Delimiter ;
-
+/*
 -- READ
 Delimiter $$
 create procedure sp_read_bibliotecario()
@@ -253,58 +301,57 @@ begin
     select * from Bibliotecario;
 end$$
 Delimiter ;
-
+*/
 -- UPDATE
 Delimiter $$
 create procedure sp_update_bibliotecario(
-    in ID_bibliotecario_p varchar(36),
+    in ID_bibliotecario_p int,
     in nombre_bibliotecario_p varchar(50),
     in apellido_bibliotecario_p varchar(50),
-    in correo_p varchar(50),
-    in contrasena_p varchar(30)
+    in correo_bibliotecario_p varchar(50),
+    in contrasena_bibliotecario_p varchar(30)
 )
 begin
     update Bibliotecario
     set
         nombre_bibliotecario = nombre_bibliotecario_p,
         apellido_bibliotecario = apellido_bibliotecario_p,
-        correo = correo_p,
-        contrasena = contrasena_p
+        correo_bibliotecario = correo_bibliotecario_p,
+        contrasena_bibliotecario = contrasena_bibliotecario_p
     where ID_bibliotecario = ID_bibliotecario_p;
 end$$
 Delimiter ;
 
 -- DELETE
 Delimiter $$
-create procedure sp_delete_bibliotecario(in ID_bibliotecario_p varchar(36))
+create procedure sp_delete_bibliotecario(in ID_bibliotecario_p int)
 begin
     delete from Bibliotecario where ID_bibliotecario = ID_bibliotecario_p;
 end$$
 Delimiter ;
-
+/*
 -- SEARCH
 Delimiter $$
-create procedure sp_search_bibliotecario(in ID_bibliotecario_p varchar(36))
+create procedure sp_search_bibliotecario(in ID_bibliotecario_p int)
 begin
     select * from Bibliotecario where ID_bibliotecario = ID_bibliotecario_p;
 end$$
 Delimiter ;
-
+*/
 -- ----------------------------------CRUDS Prestamo--------------------------------------------
 
 -- CREATE
 Delimiter $$
 create procedure sp_create_prestamo(
-    in ID_prestamo_p varchar(36),
-    in ID_estudiante_p varchar(36),
+    in ID_estudiante_p int,
     in ISBN_p varchar(13),
     in fecha_adquisicion_p date,
     in fecha_vencimiento_p date,
     in aprobacion_prestamo_p boolean
 )
 begin
-    insert into Prestamo(ID_prestamo, ID_estudiante, ISBN, fecha_adquisicion, fecha_vencimiento, aprobacion_prestamo)
-    values(ID_prestamo_p, ID_estudiante_p, ISBN_p, fecha_adquisicion_p, fecha_vencimiento_p, aprobacion_prestamo_p);
+    insert into Prestamo(ID_estudiante, ISBN, fecha_adquisicion, fecha_vencimiento, aprobacion_prestamo)
+    values(ID_estudiante_p, ISBN_p, fecha_adquisicion_p, fecha_vencimiento_p, aprobacion_prestamo_p);
 end$$
 Delimiter ;
 
@@ -319,8 +366,8 @@ Delimiter ;
 -- UPDATE
 Delimiter $$
 create procedure sp_update_prestamo(
-    in ID_prestamo_p varchar(36),
-    in ID_estudiante_p varchar(36),
+    in ID_prestamo_p int,
+    in ID_estudiante_p int,
     in ISBN_p varchar(13),
     in fecha_adquisicion_p date,
     in fecha_vencimiento_p date,
@@ -340,7 +387,7 @@ Delimiter ;
 
 -- DELETE
 Delimiter $$
-create procedure sp_delete_prestamo(in ID_prestamo_p varchar(36))
+create procedure sp_delete_prestamo(in ID_prestamo_p int)
 begin
     delete from Prestamo where ID_prestamo = ID_prestamo_p;
 end$$
@@ -348,7 +395,7 @@ Delimiter ;
 
 -- SEARCH
 Delimiter $$
-create procedure sp_search_prestamo(in ID_prestamo_p varchar(36))
+create procedure sp_search_prestamo(in ID_prestamo_p int)
 begin
     select * from Prestamo where ID_prestamo = ID_prestamo_p;
 end$$
