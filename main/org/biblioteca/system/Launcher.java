0ae6d7ec4c54bbb4743506881862c2b18e12f230
bvasquez-byte <bvasquezquel@gmail.com>
2026-09-07 12:03:17 -0600

    refactor(paquetes): Cambio de nombre del TLD

--- main/org/biblioteca/system/Launcher.java
+++ main/org/biblioteca/system/Launcher.java
@@ -1,4 +1,4 @@
-package org.catalogodigital.system;
+package org.biblioteca.system;
 
 public class Launcher {
     public static void main(String[] args) {
--- main/org/biblioteca/system/Main.java
+++ main/org/biblioteca/system/Main.java
@@ -1,4 +1,4 @@
-package org.catalogodigital.system;
+package org.biblioteca.system;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
