771334963a14b6485ecc8cc91c58fa2e33dd0532
Nathan <aaguilar-2023146@kinal.edu.gt>
2026-09-08 08:55:22 -0600

    feat(SceneManager): Clase que controla el cambio de escenas; se instancia por un singleton; realiza el cambio de la escena del Stage.

---
+++ main/org/catalogodigital/system/utils/SceneManager.java
@@ -0,0 +1,36 @@
+package org.catalogodigital.system.utils;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SceneManager {
+    private static SceneManager instanciaSceneManager;
+    private Stage primaryStage;
+
+    private SceneManager() {
+    }
+
+    private Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    private void setPrimaryStage(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
+
+    public static SceneManager getInstanciaSceneManager() {
+        if (instanciaSceneManager == null)
+            instanciaSceneManager = new SceneManager();
+        return instanciaSceneManager;
+    }
+
+    public void changeScene(Scene scene) {
+        try {
+            getPrimaryStage().setScene(scene);
+            getPrimaryStage().sizeToScene();
+            getPrimaryStage().show();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
