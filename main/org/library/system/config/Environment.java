b114e5b9d35abe69f3e86fc4e2a8b3f6bc4ec188
aaguilar-2023146 <ntnl.a1216@gmail.com>
2026-09-09 11:00:03 -0600

    Delete main/org/library/system/config/Environment.java

--- main/org/library/system/config/Environment.java
+++
@@ -1,8 +0,0 @@
-package org.library.system.config;
-
-public class Environment {
-    protected final static String USER ="in4am";
-    protected static final String PASSWORD ="@dmin4AM";
-    protected static final String DATA_BASE ="library_borrowing_system_in4am";
-    protected static final String LOCAL_HOST = "localhost:3306";
-}
