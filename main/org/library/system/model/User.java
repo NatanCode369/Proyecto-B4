ef302066cc37aa108b897c6b5712f1c51313c3d0
Nathan <aaguilar-2023146@kinal.edu.gt>
2026-09-14 11:45:36 -0600

    refactor: correción de ubicación de paquetes.

---
+++ main/org/library/system/model/User.java
@@ -0,0 +1,44 @@
+package org.library.system.model;
+
+import org.library.system.enums.Role;
+
+public class User {
+    private Integer user_id;
+    private String user_code;
+    private String first_name;
+    private String last_name;
+    private String email;
+    private String password_hash;
+    private Role user_role;
+    private Boolean active;
+
+    public User() {}
+
+    public User(Integer user_id, String user_code, String first_name, String last_name, String email, String password_hash, Role user_role, Boolean active) {
+        this.user_id = user_id;
+        this.user_code = user_code;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.password_hash = password_hash;
+        this.user_role = user_role;
+        this.active = active;
+    }
+
+    public Integer getUser_id() { return user_id; }
+    public void setUser_id(Integer user_id) { this.user_id = user_id; }
+    public String getUser_code() { return user_code; }
+    public void setUser_code(String user_code) { this.user_code = user_code; }
+    public String getFirst_name() { return first_name; }
+    public void setFirst_name(String first_name) { this.first_name = first_name; }
+    public String getLast_name() { return last_name; }
+    public void setLast_name(String last_name) { this.last_name = last_name; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getPassword_hash() { return password_hash; }
+    public void setPassword_hash(String password_hash) { this.password_hash = password_hash; }
+    public Role getUser_role() { return user_role; }
+    public void setUser_role(Role user_role) { this.user_role = user_role; }
+    public Boolean getActive() { return active; }
+    public void setActive(Boolean active) { this.active = active; }
+}
