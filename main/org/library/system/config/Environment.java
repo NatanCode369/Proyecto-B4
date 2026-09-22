1e1d635f6424e04e1a7d35e78fc1cae5c35b652f
franciscoalvarez <falvarez-2023239@kinal.edu.gt>
2026-09-22 09:59:16 -0600

    styles: Se cambio la distribucion de las ventanas para que fuera mas simetrico

--- main/org/library/system/config/Environment.java
+++ main/org/library/system/config/Environment.java
@@ -1,7 +1,7 @@
 package org.library.system.config;
 
 public class Environment {
-    protected final static String USER ="in4am";
+    protected final static String USER ="IN4AM";
     protected static final String PASSWORD ="@dmin4AM";
     protected static final String DATA_BASE ="library_borrowing_system_in4am";
     protected static final String LOCAL_HOST = "localhost:3306";
